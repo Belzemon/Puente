cf1c8e2001b3d67c7a4e582594f1b30df341af10
Belzemon <jbmferro99@gmail.com>
2019-11-04 00:33:15 +0100

    < Trabajo puente>

---
+++ Unity/Assets/New Folder/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rojo : MonoBehaviour {
+
+	public float velocidad = 100f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey (KeyCode.A))
+			transform.Translate (Vector2.left * velocidad * Time.deltaTime);
+
+	}
+}
+
